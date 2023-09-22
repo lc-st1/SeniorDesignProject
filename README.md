4badaa7b98eba14fb66a4c26fca75c3b9176ff6c
Luca Ciampaglia <lucasciampaglia@gmail.com>
2023-09-21 21:36:33 -0400

    ECSE398_first_commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# SeniorDesignProject
+# ECSE 398 Senior Design Project: Automatic Guitar Tuner
+
+### Luca Ciampaglia, Daniel French, Andrew Li
+
+This is the repository for the Automatic Guitar Tuner Project for ECSE 398.
+
+This contains the arduino code as well as any other necessary files.
---
+++ code/automatic_guitar_tuner_code.ino
@@ -0,0 +1,45 @@
+// Automatic Guitar Tuner Arduino Main Code
+// Luca Ciampaglia, Daniel French, Andrew Li
+// ECSE 398 Senior Design Project
+
+
+// Include any necessary libraries
+
+void setup() {
+
+    // Read library of guitar tuning setups, string notes, and associated frequencies
+
+    // Initialize stuff for the frequency detection
+
+    // Initialize the motor controller
+
+}
+
+void loop() {
+
+    // Wait for user input to select a desired tuning setup
+
+    // Update screen with selected tuning and basic instructions
+
+    // Entire Tuning Loop:
+
+        // Wait for frequency reading to begin PID tuning loop
+
+        // Individual String Tuning Loop:
+
+            // Read current frequency
+            
+            // Apply digital filtering
+
+            // Calculate frequnecy error
+
+            // Actuate motor based on error
+
+            // Break once string is within frequency tolerance
+
+        // Update screen with next instructions 
+
+        //  Move to next string note or break if all strings have been tuned
+
+        
+}
